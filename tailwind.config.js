a81e26160a4032edbd7ad558bfe582b0d1837697
Siddhartha-Subudhi <sssubudhi23@gmail.com>
2025-08-15 21:34:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: { extend: { colors: { brandNavy: "#0B1D39", brandGold: "#FFD369", brandTeal: "#14B8A6" } } },
+  plugins: [],
+};
